0693a6c4a734ba609fc11448a377ff53637b408b
Adrian Cirstei <adicirstei@gmail.com>
2016-05-02 21:47:30 +0300

    commented out

--- Drawing.fsx
+++ Drawing.fsx
@@ -1,13 +1,11 @@
-open System.Drawing
-
-let i = new Bitmap(300,300)
-let g = Graphics.FromImage i
-
-let br = new SolidBrush(Color.IndianRed)
-
-g.FillRectangle(br, 0,0,300,300)
-g.Flush()
-
+//open System.Drawing
+//
+//let i = new Bitmap(300,300)
+//let g = Graphics.FromImage i
+//
+//let br = new SolidBrush(Color.IndianRed)
+//
+//g.FillRectangle(br, 0,0,300,300)
+//g.Flush()
+//
 //i.Save "x.png"
-    
-System.Windows.Forms.Clipboard.SetImage(i)
