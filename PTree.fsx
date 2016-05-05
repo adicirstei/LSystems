9cda8a0254a01ae628781c08b14f5a341621b3a4
Adrian <adicirstei@gmail.com>
2016-05-05 16:26:44 +0300

    ptree

--- PTree.fsx
+++ PTree.fsx
@@ -1,9 +1,16 @@
 #load "LSystem.fsx"
+#load "Drawing.fsx"
 
 let ptree:LSystem.LSystem = {
     axiom = ['0']
     rules = Map.ofList ['1', ['1'; '1']
                         '0', ['1'; '['; '0'; ']'; '0' ]]
 }
+
+open Drawing
+
+let render (state: LSystem.State) : Line list =
+    []
+
 
 LSystem.evolveTo ptree 4
