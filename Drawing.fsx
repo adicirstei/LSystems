39f0ea8e8f707aeb491564bcd0fa3006a5aa4f05
Adrian <adicirstei@gmail.com>
2016-05-05 10:13:46 +0300

    some more things

--- Drawing.fsx
+++ Drawing.fsx
@@ -1,3 +1,24 @@
+type Position = float*float
+
+type Rotation = float
+
+type Length = float
+
+type Stack = (Position*Rotation) list
+
+let push (x:(Position*Rotation)) (stack:Stack) : Stack =
+    x::stack
+
+let pop ((hd::tl):Stack) : (Position*Rotation)*Stack =
+    hd,tl
+
+
+let calcEndPos (x,y) rotation length =
+    let endX = x + (length * cos rotation)
+    let endY = y + (length * sin rotation)
+    endX, endY
+
+
 //open System.Drawing
 //
 //let i = new Bitmap(300,300)
--- LSystem.fsx
+++ LSystem.fsx
@@ -5,35 +5,15 @@ type LSystem = {
     axiom : State
     rules : Rules
 }
-    
-type Position = float*float    
-    
-type Rotation = float
 
-type Length = float
 
-type Stack = (Position*Rotation) list   
-    
 let evolve (rules:Rules) (state:State) : State =
     state
-    |> List.map (fun s -> defaultArg (Map.tryFind s rules) [s]) 
+    |> List.map (fun s -> defaultArg (Map.tryFind s rules) [s])
     |> List.concat
-    
+
 let evolveTo (ls:LSystem) (gen:int) : State =
     let rec loop n state =
         if n>= gen then state
         else evolve ls.rules state |> loop (n+1)
     loop 0 ls.axiom
-    
-    
-let push (x:(Position*Rotation)) (stack:Stack) : Stack =
-    x::stack
-    
-let pop ((hd::tl):Stack) : (Position*Rotation)*Stack =
-    hd,tl
-    
-    
-let calcEndPos (x,y) rotation length =
-    let endX = x + (length * cos rotation) 
-    let endY = y + (length * sin rotation)
-    endX, endY
